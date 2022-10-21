ad33f04778e0588dbe54cead15f99e544c50ce4d
lvingman <45737515+lvingman@users.noreply.github.com>
2022-10-20 21:41:59 -0300

    Agrega popup de cantidad de items en el carrito

--- Carrito/Site.Master.cs
+++ Carrito/Site.Master.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using dominio;
+using negocio;
 
 namespace Carrito
 {
     public partial class SiteMaster : MasterPage
     {
+        public List<ItemCarrito> listaArticulos { get; set; }
+        
+        public int itemsCarro { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+          
         }
+
+    
+        public int CarritoVacio()
+        {
+            int aDevolver = 0;
+            List<ItemCarrito> cantItems = Session["listaEnCarro"] != null ?
+                (List<ItemCarrito>)Session["listaEnCarro"] : new List<ItemCarrito>();
+            foreach (ItemCarrito item in cantItems)
+            {
+                aDevolver += item.Cantidad;
+            }
+            return aDevolver;
+        }
+       
+
+
     }
 }
